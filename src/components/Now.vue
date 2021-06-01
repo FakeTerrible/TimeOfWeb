0bf9f9f203ef576a8c8c5be7a700d68815818ac4
FakeTerrible <1256325972@qq.com>
2021-06-01 17:18:04 +0800

    first

---
+++ src/components/Now.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="time">
+    {{ time.toLocaleTimeString() }}
+    <br>
+    {{ time.toLocaleDateString() }}
+  </div>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted, reactive, ref } from "vue";
+
+const time = ref(new Date());
+const obj = reactive({ time });
+
+onMounted(function () {
+  this.timer = setInterval(function () {
+    obj.time = new Date();
+  }, 1000);
+});
+
+onUnmounted(function(){
+  if(this.timer){
+    clearInterval(this.timer);
+  }
+})
+</script>
+
+<style scoped>
+.time {
+  text-align: center;
+}
+</style>
